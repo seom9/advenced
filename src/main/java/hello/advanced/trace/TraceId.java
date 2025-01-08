4cfb60a649ca896794bc7a6743dbc726b3fc60dd
seom9 <dmssid78@hanmail.com>
2025-01-08 21:43:40 +0900

    20250108 일단 해 봐

--- src/main/java/hello/advanced/trace/TraceId.java
+++ src/main/java/hello/advanced/trace/TraceId.java
@@ -2,6 +2,7 @@ package hello.advanced.trace;
 
 import java.util.UUID;
 
+// 기반 데이터 트랜잭션 아이디랑 level 를 가지고 잇는 것
 public class TraceId {
 
     private String id;
@@ -14,10 +15,12 @@ public class TraceId {
 
     private String createId() {
         return UUID.randomUUID().toString().substring(0, 8);
+        // 유효 아이디
     }
 
     private TraceId createNextId() {
         return new TraceId(id, level + 1);
+        // traceId 는 똑같고 레벨은 하나 증가
     }
 
     private TraceId createPreviousId() {
@@ -28,12 +31,11 @@ public class TraceId {
         return level == 0;
     }
 
-    public String getId() {
-        return id;
-    }
-
     public int getLevel() {
         return level;
     }
 
+    public String getId() {
+        return id;
+    }
 }
